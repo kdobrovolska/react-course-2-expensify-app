7bbaeaec445b7f8aa977e5d7c97b56484a478f08
kdobrovolska <cat1960m@yahoo.com>
2018-03-29 18:08:07 +0300

    Production WebPack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 
 const path=require('path');
+const ExtractTextPlugin=require('extract-text-webpack-plugin'); 
 
 module.exports=(env)=>{
     console.log('env', env);
     const isProduction=(env==='production');
+    const CSSExtract = new ExtractTextPlugin('styles.css');
   
     return {
     entry: './src/app.js',
@@ -20,14 +22,28 @@ module.exports=(env)=>{
         },
         {
             test: /\.s?css$/,
-            use:[
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
+            use: CSSExtract.extract({
+               use: [
+                   {
+                        loader:'css-loader',
+                        options: {
+                            sourceMap:true
+                        }
+                   },
+                   {
+                        loader:'sass-loader',
+                        options: {
+                            sourceMap:true
+                        }
+                    }
+                  ]
+            })
         }]
     },
-    devtool: isProduction?'source-map':'cheap-module-eval-source-map',
+    plugins:[
+        CSSExtract
+    ],
+    devtool: isProduction?'source-map':'inline-source-map',
     devServer: {
         contentBase:path.join(__dirname,'public'),
         historyApiFallback:true
